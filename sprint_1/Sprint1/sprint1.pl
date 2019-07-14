d4f651feeea6f51a8dbdb775d04de8fee122e8c8
Lorenzo Mustich <lorenzomustich@hotmail.it>
2019-07-14 22:20:56 +0200

    Sprint1

---
+++ sprint_1/Sprint1/sprint1.pl
@@ -0,0 +1,7 @@
+%====================================================================================
+% sprint1 description   
+%====================================================================================
+context(ctxrobot, "localhost",  "TCP", "8005" ).
+ qactor( robot, ctxrobot, "it.unibo.robot.Robot").
+  qactor( maitre, ctxrobot, "it.unibo.maitre.Maitre").
+  qactor( fridge, ctxrobot, "it.unibo.fridge.Fridge").
